5b90f5fca78f1a7e4763f30c46fb6ed3c7a3167c
Noah Stevens <stevens_nas@yahoo.com>
2025-03-29 18:55:15 -0400

    name change to cool new codename

---
+++ src/main/java/io/github/skeffy/octave/OctaveApplication.java
@@ -0,0 +1,13 @@
+package io.github.skeffy.octave;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OctaveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OctaveApplication.class, args);
+	}
+
+}
