36bc83509d5e7138f058bca2f65f66f57510fb08
Noah Stevens <stevens_nas@yahoo.com>
2025-03-29 20:56:11 -0400

    Basic method signatures for user manipulation

--- src/main/java/io/github/skeffy/octave/dao/UserDao.java
+++ src/main/java/io/github/skeffy/octave/dao/UserDao.java
@@ -1,4 +1,14 @@
 package io.github.skeffy.octave.dao;
 
+import io.github.skeffy.octave.model.User;
+
 public interface UserDao {
+
+    User getUserByEmail(String email);
+
+    User createUser(User newUser);
+
+    int deleteUser(User user);
+
+    int updateUser(User user);
 }
