b83897828a2f873d2a3c1de2de740c4df2b251cd
Noah Stevens <stevens_nas@yahoo.com>
2025-03-29 20:56:44 -0400

    Implement getUserByEmail

--- src/main/java/io/github/skeffy/octave/dao/JdbcUserDao.java
+++ src/main/java/io/github/skeffy/octave/dao/JdbcUserDao.java
@@ -1,4 +1,57 @@
 package io.github.skeffy.octave.dao;
 
-public class JdbcUserDao {
+import io.github.skeffy.octave.exception.DaoException;
+import io.github.skeffy.octave.model.User;
+import org.springframework.jdbc.CannotGetJdbcConnectionException;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.support.rowset.SqlRowSet;
+import org.springframework.stereotype.Component;
+
+@Component
+public class JdbcUserDao implements UserDao{
+
+    private final JdbcTemplate jdbcTemplate;
+
+    public JdbcUserDao(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    @Override
+    public User getUserByEmail(String email) {
+        User user = null;
+        String sql = "SELECT * FROM user WHERE email = ?";
+        try {
+            SqlRowSet results = jdbcTemplate.queryForRowSet(sql, email);
+            if (results.next()) {
+                user = mapRowToUser(results);
+            } else {
+                return null;
+            }
+        }
+        catch (CannotGetJdbcConnectionException e) {
+            throw new DaoException("Unable to connect to database", e);
+        }
+        return user;
+    }
+
+    @Override
+    public User createUser(User newUser) {
+        return null;
+    }
+
+    @Override
+    public int deleteUser(User user) {
+        return 0;
+    }
+
+    @Override
+    public int updateUser(User user) {
+        return 0;
+    }
+
+    private User mapRowToUser(SqlRowSet results) {
+        User user = new User();
+
+        return user;
+    }
 }
