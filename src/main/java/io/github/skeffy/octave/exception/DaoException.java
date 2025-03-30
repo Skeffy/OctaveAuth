2bdebac655afda945c93a3eec82c1d88f90df35b
Noah Stevens <stevens_nas@yahoo.com>
2025-03-29 20:55:16 -0400

    Basic exception for DAO errors

--- src/main/java/io/github/skeffy/octave/exception/DaoException.java
+++ src/main/java/io/github/skeffy/octave/exception/DaoException.java
@@ -1,4 +1,13 @@
 package io.github.skeffy.octave.exception;
 
-public class DaoException {
+public class DaoException extends RuntimeException{
+    public DaoException() {
+        super();
+    }
+    public DaoException(String message) {
+        super(message);
+    }
+    public DaoException(String message, Exception cause) {
+        super(message, cause);
+    }
 }
