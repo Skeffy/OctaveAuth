559eb145a26ee3d65ec863cf4e3ff766cd324e71
Noah Stevens <stevens_nas@yahoo.com>
2025-04-02 20:45:02 -0400

    Created service to send access tokens to google API to retrieve user info

--- src/main/java/io/github/skeffy/octave/security/googleoauth/GoogleUserService.java
+++ src/main/java/io/github/skeffy/octave/security/googleoauth/GoogleUserService.java
@@ -1,4 +1,27 @@
 package io.github.skeffy.octave.security.googleoauth;
 
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.web.client.RestTemplate;
+
 public class GoogleUserService {
+
+    public static String API = "https://oauth2.googleapis.com/tokeninfo?access_token=";
+    private final RestTemplate restTemplate = new RestTemplate();
+
+    public GoogleResponseDto get(String token) {
+        GoogleResponseDto user = null;
+        HttpHeaders headers = new HttpHeaders();
+        HttpEntity<?> entity = new HttpEntity<>(headers);
+
+        HttpEntity<GoogleResponseDto> response = restTemplate.exchange(
+                API+token,
+                HttpMethod.GET,
+                entity,
+                GoogleResponseDto.class
+        );
+        user = response.getBody();
+        return user;
+    }
 }
