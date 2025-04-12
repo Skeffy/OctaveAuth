a331a2e643d47348b149037feb8c576dc107a8bd
Noah Stevens <stevens_nas@yahoo.com>
2025-04-12 15:36:49 -0400

    General secutriy fun

--- src/main/java/io/github/skeffy/octave/security/UserModelDetailsService.java
+++ src/main/java/io/github/skeffy/octave/security/UserModelDetailsService.java
@@ -1,4 +1,46 @@
 package io.github.skeffy.octave.security;
 
-public class UserModelDetailsService {
+import io.github.skeffy.octave.dao.UserDao;
+import io.github.skeffy.octave.model.Authority;
+import io.github.skeffy.octave.model.User;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
+@Component("userDetailsService")
+public class UserModelDetailsService implements UserDetailsService {
+
+    private final Logger log = LoggerFactory.getLogger(UserModelDetailsService.class);
+
+    private final UserDao userDao;
+
+    public UserModelDetailsService(UserDao userDao) {
+        this.userDao = userDao;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(final String login) {
+        log.debug("Authenticating user '{}'", login);
+        return createSpringSecurityUser(userDao.getUserByUsername(login));
+    }
+
+    private org.springframework.security.core.userdetails.User createSpringSecurityUser(User user) {
+        List<GrantedAuthority> grantedAuthorities = new ArrayList<>();
+        Set<Authority> userAuthorities = user.getAuthorities();
+        for (Authority authority : userAuthorities) {
+            grantedAuthorities.add(new SimpleGrantedAuthority(authority.getName()));
+        }
+
+        return new org.springframework.security.core.userdetails.User(user.getUsername(),
+                user.getId(),
+                grantedAuthorities);
+    }
 }
