5d5a5530a3b22f8a3295330b59af13d85771234e
Noah Stevens <stevens_nas@yahoo.com>
2025-04-12 13:36:44 -0400

    POJO for basic user info recieved from frontend during account registration

--- src/main/java/io/github/skeffy/octave/model/RegisterUserDto.java
+++ src/main/java/io/github/skeffy/octave/model/RegisterUserDto.java
@@ -1,4 +1,43 @@
 package io.github.skeffy.octave.model;
 
+import jakarta.validation.constraints.NotEmpty;
+
 public class RegisterUserDto {
+
+    @NotEmpty
+    private String username;
+    @NotEmpty
+    private String email;
+    @NotEmpty
+    private String name;
+
+    public RegisterUserDto(String username, String email, String name) {
+        this.username = username;
+        this.email = email;
+        this.name = name;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
